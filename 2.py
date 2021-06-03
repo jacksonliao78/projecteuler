2ba4e6832f68dabc9f228955ce3d7b7981a23567
Jackson Liao <jliao@wliao17.local>
2021-06-02 21:33:46 -0400

    even fibonacci numbers

---
+++ 2.py
@@ -0,0 +1,12 @@
+num1 = 1
+num2 = 1
+num3 = 0
+result = 0
+while result <= 4000000:
+        num3 = num2 + num1
+        if num3%2 == 0:
+                result = result + num3
+
+        num1 = num2
+        num2 = num3
+print (result)
