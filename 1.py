b6dea5492d2fb7afcf3ca7605e1aaaab3a1684bb
Jackson Liao <jliao@wliao17.local>
2021-06-02 21:32:18 -0400

    multiples of 3 and 5

---
+++ 1.py
@@ -0,0 +1,11 @@
+def work(num):
+    sum = 0
+    for n in range(0, num):
+        if n % 3 == 0 or n % 5 == 0:
+            sum = sum + n
+
+    print(sum)
+    
+
+if __name__ == "__main__":
+    work(10000)
