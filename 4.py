bbe73515de9af6c0a956fce90bfa158a71a09c0c
Jackson Liao <jliao@wliao17.local>
2021-12-23 19:11:42 -0500

    largest palindrome

--- 4.py
+++ 4.py
@@ -1,3 +1,13 @@
 
 a = 999
-b = 999
+n = 0
+for b in range(a, 100, -1):
+    for c in range(b, 100, -1):
+        d = b * c
+        if d > n:
+            pos = str(d)
+            if pos == pos[::-1]:
+                print(b,c)
+                break
+        
+   
