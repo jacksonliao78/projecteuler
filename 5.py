dc109beec4032f001aa1f666a5e4907b7012734e
Jackson Liao <jliao@wliao17.local>
2021-12-28 09:06:57 -0500

    smallest multiple

--- 5.py
+++ 5.py
@@ -1,10 +1,18 @@
 
-x= 2
-for i in range(1,10000,+1):
-    while x < 20:
-        if i % x == 0:
-            x += 1
-print(i)
-        
-
+x = 2520
+for x in range(2520, 1000000000, +2520):
+    if x % 11 == 0:
+        if x % 12 == 0:
+            if x % 13== 0:
+                if x % 14 ==0:
+                    if x % 15 == 0:
+                        if x % 16 == 0:
+                            if x % 17 == 0:
+                                if x % 18 == 0:
+                                    if x % 19 == 0:
+                                        if x % 20 == 0:
+                                            print(x)
+                                            break
+                                    
+                                    
         
