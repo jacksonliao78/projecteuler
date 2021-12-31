c243cae8c73bd3908f7c6b051f21c92fb9202315
Jackson Liao <jliao@wliao17.local>
2021-12-31 08:47:11 -0500

    sum of primes under _

--- 10.py
+++ 10.py
@@ -1,14 +1,14 @@
 
 def prime(n):
-    mult = []
+    mult = set()
     primes = []
     tot = 0
     for i in range(2, n):
         if i not in mult:
-            tot += i
-            primes.append(i)
             for j in range(i ** 2, n, +i):
-                mult.append(j)
+                mult.add(j)
+            primes.append(i)
+            tot += i
     print(tot)
     
 print(prime(2000000))
