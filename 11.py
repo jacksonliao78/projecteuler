09f40191dbc6efc87cfdc8b6b2316c80ce4b9bd8
Jackson Liao <jliao@wliao17.local>
2022-01-03 14:51:48 -0500

    adjacent products

--- 11.py
+++ 11.py
@@ -1,6 +1,7 @@
 
 nums = [[8,2,22,97,38,15,0,40,0,75,4,5,7,78,52,12,50,77,91,8],
 [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,4,56,62,0],
+[81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,3,49,13,36,65],
 [52,70,95,23,4,60,11,42,69,24,68,56,1,32,56,71,37,2,36,91],
 [22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80],
 [24,47,32,60,99,3,45,2,44,75,33,53,78,36,84,20,35,17,12,50],
@@ -19,24 +20,37 @@ nums = [[8,2,22,97,38,15,0,40,0,75,4,5,7,78,52,12,50,77,91,8],
 [20,73,35,29,78,31,90,1,74,31,49,71,48,86,81,16,23,57,5,54],
 [1,70,54,71,83,51,54,69,16,92,33,48,61,43,52,1,89,19,67,48]]
 
-prodlr = []
-produd = []
-proddiag = []
+prods = []
 prod = 1
 
 
 for i in range(0,19):
-    for j in range(0,15):
+    for j in range(0,16):
         for k in range(0,4):
             prod *= nums[i][j+k]
-        prodlr.append(prod)
+        prods.append(prod)
         prod = 1
 
-for i in range(0,15):
+for i in range(0,16):
     for j in range(0,19):
         for k in range(0,4):
             prod *= nums[i+k][j]
-        produd.append(prod)
+        prods.append(prod)
         prod = 1
 
-print(max(prodlr), max(produd))        
+for i in range(0,16):
+    for j in range(0,16):
+        for k in range(0,4):
+            prod *= nums[i+k][j+k]
+        prods.append(prod)
+        prod = 1
+
+for i in range(0,16):
+    for j in range(19,3):
+        for k in range(0,4):
+            prod *= nums[i+k][j-k]
+        prods.append(prod)
+        prod = 1
+
+print(max(prods))
+#print(nums[0][0])
