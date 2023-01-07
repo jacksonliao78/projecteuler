6a9cef20440f47e8df66c8721874daa8e6dacc4f
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-07 13:02:25 -0500

    very nice

---
+++ 29.py
@@ -0,0 +1,10 @@
+
+def solve():
+    vals = []
+    for i in range(2, 101):
+        for j in range(2, 101):
+            vals.append(i ** j)
+    vals = list(set(vals))
+    print(len(vals))
+
+solve()
