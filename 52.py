45848261f7b3f18a94f38289635e80756cc8259d
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-14 19:10:18 -0500

    very super nice epic

---
+++ 52.py
@@ -0,0 +1,24 @@
+
+def samedigits(num):
+    if str(num)[0] != '1':
+        return False
+    digits = []
+    for digit in str(num):
+        digits.append(digit)
+    for i in range(2, 7):
+        num2 = num * i
+        for digit in str(num2):
+            if digit not in digits:
+                return False
+    return True
+    
+
+def solve():
+    i = 0
+    while True:
+        i += 1
+        if samedigits(i) == True:
+            print(i)
+            break
+        
+solve()
